b4614cf36ecf9d89f1386403541f6f60013bb2ae
Aleksandr Burdakov <qwerty.burdakov@gmail.com>
2022-06-25 10:58:17 +1000

    add method toggleVisibleDb and modify writeYourGenres

--- index.js
+++ index.js
@@ -1,79 +1,84 @@
 "use strict";
 
-let numberOfFilms;
+
 
 const personalMovieDb = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true,
+
+    start: function() {
+        personalMovieDb.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+        while (this.count == '' || isNaN(this.count) || this.count == null)  {
+            this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+
+    getAnswers: function() {
+        const film = prompt('Один из последних просмотренных фильмов', '');
+        const grade = +prompt('На сколько вы оцениваете его?', '');
+    
+        if (film != null && grade != null && film != '' && grade != '' && film != film.length < 60 && grade < 100) {
+            personalMovieDb.movies[film] = grade;
+        } else {
+            alert('error');
+            this.getAnswers();
+        }
+    },
+
+    showYourLevel: function() {
+        if (this.count <= 10) {
+            alert ('Просмотренно слишком мало фильмов');
+        } else if (this.count > 10 && this.count < 31) {
+            alert ('вы классический зритель');
+        } else if (this.count >= 31) {
+            alert ('вы киноман');
+        } else {
+            alert ('неверный ответ');   
+        }
+    },
+
+    showMyDb: function() {
+        if (this.privat === false) {
+            console.log(personalMovieDb);
+        }
+
+        this.toggleVisibleMyDb();
+    },
+
+    writeYourGenres: function() {
+        for (let i = 1; i < 2; i++) {
+            let genres = prompt(`Ваш любимые жанры чере запятую`);
+
+            if (genres === '' || genres === null) {
+                i--;
+            } else {
+                this.genres = genres.split(', ');
+            }
+        }
+     
+        this.genres.forEach(function(item, i){
+            console.log(`Любимый жанр #${i + 1} - это ${item}`);
+        });
+    },
+
+    toggleVisibleMyDb: function() {
+        if (this.privat) {
+            this.privat = false;
+        } else {
+            this.privat = true;
+        }
+    }
 };
 
-function start() {
-    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || isNaN(numberOfFilms) || numberOfFilms == null)  {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
-
-function getAnswers() {
-    const film = prompt('Один из последних просмотренных фильмов', '');
-    const grade = +prompt('На сколько вы оцениваете его?', '');
-
-    if (film != null && grade != null && film != '' && grade != '' && film != film.length < 60 && grade < 100) {
-        personalMovieDb.movies[film] = grade;
-    } else {
-        alert('error');
-        getAnswers();
-    }
-}
-
-getAnswers();
-getAnswers();
-
-function showYourLevel() {
-    if (numberOfFilms <= 10) {
-        alert ('Просмотренно слишком мало фильмов');
-    } else if (numberOfFilms > 10 && numberOfFilms < 31) {
-        alert ('вы классический зритель');
-    } else if (numberOfFilms >= 31) {
-        alert ('вы киноман');
-    } else {
-        alert ('неверный ответ');   
-    }
-}
-
-showYourLevel();
-
-function showMyDb() {
-    if (personalMovieDb.privat === false) {
-        console.log(personalMovieDb);
-    }
-}
-
-showMyDb();
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDb.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
+personalMovieDb.start();
+personalMovieDb.getAnswers();
+personalMovieDb.getAnswers();
+personalMovieDb.showYourLevel();
+personalMovieDb.showMyDb();
+personalMovieDb.writeYourGenres();
 
 
-const soldier = {
-    health: '100',
-    armor: '200',
-    action: function() {
-        console.log('Открываю огонь');
-    }
-};
-
-const john = Object.create(soldier);
-
-john.action();
